ac9b5201eac6028876c7be1b1de838b0e64ddbfc
Jhonatan Henkel <jhowrf@gmail.com>
2024-02-18 11:38:36 -0300

    feat: styling list transactions

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_course_personal_expense_app/models/transactions.dart';
+import 'package:intl/intl.dart';
 
 main() {
   runApp(const ExpensesApp());
@@ -46,7 +46,49 @@ class MyHomePage extends StatelessWidget {
           Column(
             children: _transactions.map((transaction) {
               return Card(
-                child: Text(transaction.title),
+                child: Row(
+                  children: <Widget>[
+                    Container(
+                      margin: const EdgeInsets.symmetric(
+                        vertical: 10,
+                        horizontal: 15
+                      ),
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          color: Colors.purple,
+                          width: 2
+                        )
+                      ),
+                      padding: const EdgeInsets.all(10),
+                      child: Text(
+                        'R\$ ${transaction.value.toStringAsFixed(2)}',
+                        style: const TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                          color: Colors.purple,
+                        ),
+                      ),
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          transaction.title,
+                          style: const TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold
+                          ),
+                        ),
+                        Text(
+                          DateFormat('d/MM/y').format(transaction.date),
+                          style: const TextStyle(
+                            color: Colors.grey
+                          ),
+                        )
+                      ],
+                    )
+                  ],
+                ),
               );
             }).toList(),
           )
